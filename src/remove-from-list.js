5e528b97d57f77a1e7f329ff5f8c85d5c8c815e5
sarafashka <96177099+sarafashka@users.noreply.github.com>
2022-05-02 23:55:33 +0400

    feat: linked list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+ const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,10 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function removeKFromList(l, k) {
+  let head = l;
+   if (l.value === k) {
+    l = l.next
+   } 
+    while (head.next !==null) {
+      if (head.next.value === k) {
+        head.next = head.next.next;
+      } else {
+        head = head.next;
+      } 
+    }
+    return l
+  }
+  
+
+
 
 module.exports = {
   removeKFromList
